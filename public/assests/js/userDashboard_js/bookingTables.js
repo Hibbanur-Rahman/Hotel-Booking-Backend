8ec76b3d3888ca8d3a209fc5d14498a9ae633d8a
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2023-11-26 03:32:45 +0530

    add the myBooking page

---
+++ public/assests/js/userDashboard_js/bookingTables.js
@@ -0,0 +1,23 @@
+new DataTable('#example', {
+    columnDefs: [
+        {
+            targets: [0],
+            orderData: [0, 1]
+        },
+        {
+            targets: [1],
+            orderData: [1, 0]
+        },
+        {
+            targets: [4],
+            orderData: [4, 0]
+        }
+    ]
+  });
+  let table = new DataTable('#example');
+   
+  table.on('click', 'tbody tr', function () {
+      let data = table.row(this).data();
+   
+      alert('You clicked on ' + data[0] + "'s row");
+  });
