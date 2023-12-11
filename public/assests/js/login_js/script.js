55d5037b92d377fcf99fd3acc21cfc23cfe2f862
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2023-12-11 17:20:32 +0530

    add the login eye drop and the repeated password checkup

--- public/assests/js/login_js/script.js
+++ public/assests/js/login_js/script.js
@@ -3,7 +3,29 @@ var loginAnchor = document.querySelector(".loginAnchor");
 var registerAnchor = document.querySelector(".registerAnchor");
 var form = document.querySelectorAll("form");
 
-console.log(form);
+var eyeOpen = document.querySelectorAll(".password i");
+var passwordFeild = document.querySelectorAll(".password input");
+var registerPassword = document.querySelector("#registerPassword");
+var registerRepeatPassword = document.querySelector("#registerRepeatPassword");
+var matchingStatement = document.querySelector(".matchingStatement");
+
+var signUpBtn = document.querySelector("#signUp");
+console.log(signUpBtn);
+
+registerRepeatPassword.addEventListener("blur", (event) => {
+  if (registerPassword.value != registerRepeatPassword.value) {
+    console.log("not matched");
+    signUpBtn.type = "text";
+    signUpBtn.setAttribute("disabled", "");
+    matchingStatement.innerHTML = "password dosen't match";
+    console.log(event.currentTarget.parentElement);
+  } else {
+    signUpBtn.type = "submit";
+    signUpBtn.removeAttribute("disabled");
+    matchingStatement.innerHTML = "";
+    
+  }
+});
 
 function OnToast() {
   tabPane[1].classList.remove("active");
@@ -23,5 +45,16 @@ function OnToast() {
     dismissable: true,
     theme: "glass",
   });
-  
 }
+
+//eye oppener and password hide and show implementations
+eyeOpen.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    event.currentTarget.nextElementSibling.type == "password"
+      ? (event.currentTarget.nextElementSibling.type = "text")
+      : (event.currentTarget.nextElementSibling.type = "password");
+    event.currentTarget.classList.contains("la-eye-slash")
+      ? event.currentTarget.classList.remove("la-eye-slash")
+      : event.currentTarget.classList.add("la-eye-slash");
+  });
+});
