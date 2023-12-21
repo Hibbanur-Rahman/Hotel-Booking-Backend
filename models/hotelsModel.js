a52df2596496e90360c734f9363f1565ff5f265b
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2023-12-22 03:01:03 +0530

    add the hotel model in models folder

---
+++ models/hotelsModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const hotelSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required:true,
+  },
+  pin:{
+    type: Number,
+    required:true,
+  },
+  image_hotel:{
+    type:String,
+    required:true,
+  },
+  description:{
+    type: String,
+    required: true,
+  },
+
+});
+
+
+module.exports=mongoose.model('hotels',hotelSchema);
