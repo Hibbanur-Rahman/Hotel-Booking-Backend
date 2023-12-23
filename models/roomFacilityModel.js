76ac9b81aa8acb3d2b34091b60794f6ed36c2645
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2023-12-23 16:22:14 +0530

    add the rromFacilityModel in model folder

--- models/roomFacilityModel.js
+++ models/roomFacilityModel.js
@@ -1,12 +1,18 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-const roomFacilitySchema= new mongoose.Schema({
-    id:{
-        type:String,
-        required: true,
-    },
-    roomId:{
-        type:String,
-        required: true,
-    },
-})
+const roomFacilitySchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  roomId: {
+    type: String,
+    required: true,
+  },
+  amentitiesId: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports=mongoose.model('roomFacility',roomFacilitySchema);
