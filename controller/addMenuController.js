856fd0dfa1ae5fdee46cc17d01e8bb1b362f2228
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2023-12-30 20:17:51 +0530

    trying to adding to menuItems adding controller in the addlisting part in admin Dashboard

---
+++ controller/addMenuController.js
@@ -0,0 +1,9 @@
+const httpStatusCodes=require('../constants/httpStatusCodes');
+const MenuItemCollection=require('../models/menuItemsModel');
+
+
+modules.exports.AddMenuItem=async (req,res)=>{
+    const {ItemName,ItemCategory,ItemPrice,ItemDescription}=req.body;
+    
+
+}
