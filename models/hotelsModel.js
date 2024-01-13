e2efcdc6e8da1996c190b18dc69b7d639207ba1d
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-01-13 17:05:19 +0530

    implement the multer middleware

--- models/hotelsModel.js
+++ models/hotelsModel.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const hotelSchema = new mongoose.Schema({
+  userId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User',
+  },
   hotelId: {
     type: Number,
     required: true,
